550e392b91de4ae714db98e592eb89b5fb839207
nazmieievilya <ilyanazmeev111@gmail.com>
2022-12-08 13:04:38 +0300

    changed readme file

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ function mobileMenuClickHandler() {
   header.classList.toggle("nav-open");
 }
 menuBtn.addEventListener("click", mobileMenuClickHandler);
+const yearEl = document.querySelector(".year");
+const currYear = new Date().getFullYear();
+yearEl.textContent = currYear;
